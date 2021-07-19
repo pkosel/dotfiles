5daeb8f3d2e5906b52a010a7a22bb7b79aad663a
Philipp Kosel <pkosel@fastmail.com>
2021-07-19 12:49:00 +0200

    Add active window to polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Terminate already running bar instances
+killall -q active-window.sh
 killall -q polybar
 
 # Wait until the processes have been shut down
+while pgrep -u $UID -x active-window.sh >/dev/null; do sleep 1; done
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
+$HOME/.config/polybar/active-window.sh &
 polybar mybar &
 
