8b7b15cb536e7629e1f98722842230a978015f3c
Philipp Kosel <pkosel@fastmail.com>
2021-12-22 16:24:22 +0100

    Add pacman cache cleanup script & hook

---
+++ scripts/.local/bin/yaycache.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+cachedir="/home/philipp/.cache/yay"
+
+removed="$(comm -23 <(basename -a $(find $cachedir -mindepth 1 -maxdepth 1 -type d) | sort) <(pacman -Qqm) | xargs -r printf "$cachedir/%s\n")"
+
+# Remove yay cache for foreign packages that are not installed anymore
+rm -rf $removed
+
+pkgcache="$(find $cachedir -mindepth 1 -maxdepth 1 -type d | xargs -r printf "-c %s\n")"
+
+# Remove everything for uninstalled foreign packages, keep latest version for uninstalled native packages, keep two latest versions for installed packages
+/usr/bin/paccache -rvuk0 $pkgcache
+/usr/bin/paccache -rvuk1
+/usr/bin/paccache -rvk2 -c /var/cache/pacman/pkg $pkgcache
+
